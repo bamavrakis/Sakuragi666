109c473274818a06f5041dbd20189943033844ac
Óscar Estay <oaestay@uc.cl>
2016-05-01 14:35:58 -0300

    Added avatar to users with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501052629) do
+ActiveRecord::Schema.define(version: 20160501155006) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",     null: false
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20160501052629) do
     t.string   "name",                   default: ""
     t.string   "last_name",              default: ""
     t.string   "kind",                   default: "user"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
