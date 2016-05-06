bf40fa95b06906383f2e02ed62525da4303b5d1e
Óscar Estay <oaestay@uc.cl>
2016-05-06 12:18:45 -0300

    Added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+file = File.open("app/assets/documents/test.pdf")
+User.create(email: "oscar@oscar.cl", name: "Oscar", last_name: "Estay", kind: "user", password: "password", password_confirmation: "password", avatar: File.new("app/assets/images/bad_joke.png"))
+Book.create(user_id: 1, name: "Test 0", private: true, document: file)
+Book.create(user_id: 1, name: "Test 1", private: true, document: file)
+Book.create(user_id: 1, name: "Test 2", private: true, document: file)
+Book.create(user_id: 1, name: "Test 3", private: true, document: file)
+Book.create(user_id: 1, name: "Test 4", private: true, document: file)
+Book.create(user_id: 1, name: "Test 5", private: true, document: file)
+Book.create(user_id: 1, name: "Test 6", private: false, document: file)
+Book.create(user_id: 1, name: "Test 7", private: false, document: file)
+Book.create(user_id: 1, name: "Test 8", private: false, document: file)
+Book.create(user_id: 1, name: "Test 9", private: false, document: file)
+Book.create(user_id: 1, name: "Test 10", private: false, document: file)
+Book.create(user_id: 1, name: "Test 11", private: true, document: file)
+Book.create(user_id: 1, name: "Test 12", private: true, document: file)
+
+file.close
