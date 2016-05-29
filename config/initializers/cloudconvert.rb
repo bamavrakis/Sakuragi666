04821b011f0cf9e6be5055180b140c5cb554e749
Óscar Estay <oaestay@uc.cl>
2016-05-29 15:40:43 -0400

    Commented out cloudconvert on initialiazers

--- config/initializers/cloudconvert.rb
+++ config/initializers/cloudconvert.rb
@@ -1,4 +1,4 @@
-Cloudconvert.configure do |config|
-    config.api_key  = 'mcgo6n6wlB_vfGnPwjiuxkytOtV3PIs3A1MmC-DJescayEgxqKYHu4_HWAfsCqmKssG4hdOO8K4AZnSM51oz8A'
-    config.callback = 'http://localhost:3000/'
-end
+#Cloudconvert.configure do |config|
+#    config.api_key  = 'mcgo6n6wlB_vfGnPwjiuxkytOtV3PIs3A1MmC-DJescayEgxqKYHu4_HWAfsCqmKssG4hdOO8K4AZnSM51oz8A'
+#    config.callback = 'http://localhost:3000/'
+#end
